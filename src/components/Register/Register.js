298ca18826959af92e79fcfa801a7dee2adb97fb
TanjaKuo <77611611+TanjaKuo@users.noreply.github.com>
2021-08-08 14:14:08 +1000

    u

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -23,7 +23,7 @@ class Register extends React.Component {
   };
 
   onSubmitSignIn = () => {
-    fetch("https://floating-citadel-01605.herokuapp.com/register", {
+    fetch("https://rocky-crag-43483.herokuapp.com/home", {
       method: "post",
       headers: { "Content-Type": "application/json" },
       body: JSON.stringify({
