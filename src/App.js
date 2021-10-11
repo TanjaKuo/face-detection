5221bd179abab073e954a4fc94df993cec96bfce
TanjaKuo <77611611+TanjaKuo@users.noreply.github.com>
2021-10-12 00:18:55 +1100

    update scss

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import Register from "./components/Register/Register";
 //import Logo from "./components/Logo/Logo";
 import ImageLinkForm from "./components/ImageLinkForm/ImageLinkForm";
 import Rank from "./components/Rank/Rank";
+import "./Style/app.css";
 
 const initialState = {
   input: "",
