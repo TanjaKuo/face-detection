00e7074f308a3e5102864bb530aac0351392fdab
TanjaKuo <77611611+TanjaKuo@users.noreply.github.com>
2021-10-11 23:48:34 +1100

    update scss

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from "react";
-import Particles from "react-particles-js";
+//import Particles from "react-particles-js";
 //import Clarifai from "clarifai";
 //import tachyons from "tachyons";
 import FaceRecognition from "./components/FaceRecognition/FaceRecognition";
